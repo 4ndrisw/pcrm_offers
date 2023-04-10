3ada6d80b780cac85d3096046f2f2b6b7c0fc083
4ndrisw <4ndr1sw@gmail.com>
2023-04-10 10:14:20 +0700

    Update estimate_convert_template.php

--- views/admin/offers/estimate_convert_template.php
+++ views/admin/offers/estimate_convert_template.php
@@ -1,25 +1,25 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-<div class="modal fade offer-convert-modal" id="convert_to_estimate" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade offer-convert-modal" id="convert_to_offer" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog modal-xxl" role="document">
-        <?php echo form_open('admin/offers/convert_to_estimate/'.$offer->id,array('id'=>'offer_convert_to_estimate_form','class'=>'_transaction_form disable-on-submit')); ?>
+        <?php echo form_open('admin/offers/convert_to_offer/'.$offer->id,array('id'=>'offer_convert_to_offer_form','class'=>'_transaction_form disable-on-submit')); ?>
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" onclick="close_modal_manually('#convert_to_estimate')" aria-label="Close">
+                <button type="button" class="close" onclick="close_modal_manually('#convert_to_offer')" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
                 <h4 class="modal-title" id="myModalLabel">
-                    <span class="edit-title"><?php echo _l('offer_convert_to_estimate'); ?></span>
+                    <span class="edit-title"><?php echo _l('offer_convert_to_offer'); ?></span>
                 </h4>
             </div>
             <div class="modal-body">
                 <div class="row">
                     <div class="col-md-12">
-                        <?php $this->load->view('admin/estimates/estimate_template'); ?>
+                        <?php $this->load->view('admin/offers/offer_template'); ?>
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" onclick="close_modal_manually('#convert_to_estimate')">
+                <button type="button" class="btn btn-default" onclick="close_modal_manually('#convert_to_offer')">
                     <?php echo _l('close'); ?>
                 </button>
                 <button type="submit" class="btn btn-info"><?php echo _l('submit'); ?></button>
@@ -38,9 +38,9 @@
     init_color_pickers();
     init_items_sortable();
     init_tags_inputs();
-    validate_estimate_form('#offer_convert_to_estimate_form');
+    validate_offer_form('#offer_convert_to_offer_form');
     <?php if($offer->assigned != 0){ ?>
-    $('#convert_to_estimate #sale_agent').selectpicker('val',<?php echo $offer->assigned; ?>);
+    $('#convert_to_offer #sale_agent').selectpicker('val',<?php echo $offer->assigned; ?>);
     <?php } ?>
     $('select[name="discount_type"]').selectpicker('val','<?php echo $offer->discount_type; ?>');
     $('input[name="discount_percent"]').val('<?php echo $offer->discount_percent; ?>');
@@ -50,9 +50,9 @@
     <?php } ?>
     $('input[name="adjustment"]').val('<?php echo $offer->adjustment; ?>');
     $('input[name="show_quantity_as"][value="<?php echo $offer->show_quantity_as; ?>"]').prop('checked',true).change();
-    $('#convert_to_estimate #client_id').change();
+    $('#convert_to_offer #client_id').change();
     // Trigger item select width fix
-    $('#convert_to_estimate').on('shown.bs.modal', function(){
+    $('#convert_to_offer').on('shown.bs.modal', function(){
         $('#item_select').trigger('change')
     })
 
