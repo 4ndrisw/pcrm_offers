410d2744a5f1bc03e310f699a0ba4c8dc5453f8d
4ndrisw <4ndr1sw@gmail.com>
2023-04-10 10:12:12 +0700

    Update offers_lang.php

--- language/indonesia/offers_lang.php
+++ language/indonesia/offers_lang.php
@@ -49,3 +49,26 @@ $lang['offer_lowercase'] = 'Penawaran';
 $lang['offer_send_to_email_title'] = 'judul';
 $lang['offer_attach_pdf'] = 'Attach PDF';
 $lang['offer_preview_template'] = 'Preview';
+
+
+$lang['offer_lowercase'] = 'Penawaran Harga';
+$lang['offer_send_to_email_title'] = 'Judul';
+$lang['offer_attach_pdf'] = 'Lampirkan PDF';
+$lang['offer_preview_template'] = 'Preview';
+$lang['offer_related']='Penawaran terkait';
+$lang['offer_for_lead']='Offer untuk lead';
+$lang['offer_for_customer']='Offer untuk customer';
+$lang['offer_allow_comments']='Ijinkan kommentar';
+$lang['offer_allow_comments_help']='Ijinkan kommentar';
+$lang['include_offer_items_merge_field_help']='Termasuk Penawaran item merge field';
+$lang['offer_table_hours_heading']='Jam';
+$lang['offer_table_item_description']='Deskripsi';
+$lang['offer_table_tax_heading']='Pajak';
+$lang['offer_discount']='Diskon';
+$lang['offer_adjustment']='Penyesuaian';
+$lang['offer_pdf_heading']='Penawaran Harga';
+$lang['offer_data_date']='Tanggal Dibuat';
+$lang['offer_data_expiry_date']='Tanggal Kadaluarsa';
+
+$lang['clients_accept_offer']='Terima';
+$lang['clients_decline_offer']='Tolak';
